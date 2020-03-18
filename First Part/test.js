40d2c64578607d7d36b7727008ea749c2ed7f6fd
aar <aarunov@gmail.com>
2020-03-18 22:59:16 +0300

    end first lesson

---
+++ First Part/test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+  var money = prompt ("Ваш бюджет на месяц?", ''),
+    time = prompt ("Введите дату в формате YYYY-MM-DD", '');
+
+
+  let appData = {
+    budget: money,
+      timedata: time,  
+        optionalExpenses: {},
+          expenses: {},
+            income: [],
+              savings: false
+  };
+let q1 = prompt ("введите обязательную статья расходов в этом месяце"),
+     q2 = prompt ("во сколько обойдется"),
+     q3 = prompt ("введите обязательную статья расходов в этом месяце"),
+     q4 = prompt ("во сколько обойдется");
+appData.expenses[q1]=q2;
+appData.expenses[q3]=q4; 
+
+
+alert(appData.budget/30);
+
+
